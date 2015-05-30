7e66736a047083ffa7da095aa9e4b2b5c5eb1118
stefanreichenauer <stefan-reichenauer@gmx.at>
2015-05-30 23:06:08 +0200

    Plane Control

--- ka_ata_killa/Assets/scripts/MoonMovement.cs
+++ ka_ata_killa/Assets/scripts/MoonMovement.cs
@@ -4,29 +4,31 @@ using System.Collections;
 public class MoonMovement : MonoBehaviour {
 
 	private Vector3 screenPoint;
-	private Vector3 offset;
+	//private Vector3 offset;
 	public int followMouseSpeed = 8;
-	private float radius_ref;
+	private float radius;
+	public static int dist = 0;
+	public int distance = 0;
 
 
 	void OnMouseDown(){
 		screenPoint = Camera.main.WorldToScreenPoint (transform.position);
 
-		offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-		}
+		//offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, screenPoint.y, Input.mousePosition.z));
+	}
 
 	void OnMouseDrag()
 	{
+		float radius_ref = Mathf.Clamp(radius - dist, radius-10,radius);
 		float x;
-		float y;
-		float phi = Mathf.Atan2 (transform.position.y, transform.position.x);
+		float z;
+		float phi = Mathf.Atan2 (transform.position.x, transform.position.z);
 		Vector3 mouse = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 	
-		if (mouse.y == 0 && mouse.x == 0) {
+		if (mouse.z == 0 && mouse.x == 0) {
 			return;
 		}
-
-		float phiMouse = Mathf.Atan2 (mouse.y, mouse.x);
+		float phiMouse = Mathf.Atan2 (mouse.x, mouse.z);
 
 		if (phi < 0) {
 			phi += 2 * Mathf.PI;
@@ -34,7 +36,7 @@ public class MoonMovement : MonoBehaviour {
 		if (phiMouse < 0) {
 			phiMouse += 2 * Mathf.PI;
 		}
-
+		Debug.Log (radius_ref);
 		if (Mathf.Abs (phi - phiMouse) > Mathf.PI / followMouseSpeed
 		    && Mathf.Abs (phi - phiMouse) < (2*followMouseSpeed-1)*Mathf.PI / followMouseSpeed) {
 
@@ -44,39 +46,44 @@ public class MoonMovement : MonoBehaviour {
 			{				
 				if(phi-phiMouse<0)
 				{
-					x = radius_ref * Mathf.Cos (phi-Mathf.PI / followMouseSpeed);
-					y = radius_ref * Mathf.Sin (phi-Mathf.PI / followMouseSpeed);
+					z = radius_ref * Mathf.Cos (phi-Mathf.PI / followMouseSpeed);
+					x = radius_ref * Mathf.Sin (phi-Mathf.PI / followMouseSpeed);
 				}
 				else
 				{
-					x = radius_ref * Mathf.Cos (phi+Mathf.PI / followMouseSpeed);
-					y = radius_ref * Mathf.Sin (phi+Mathf.PI / followMouseSpeed);
+					z = radius_ref * Mathf.Cos (phi+Mathf.PI / followMouseSpeed);
+					x = radius_ref * Mathf.Sin (phi+Mathf.PI / followMouseSpeed);
 				}
 			}
 			else{
 				if(phi-phiMouse<0)
 				{
-					x = radius_ref * Mathf.Cos (phi+Mathf.PI / followMouseSpeed);
-					y = radius_ref * Mathf.Sin (phi+Mathf.PI / followMouseSpeed);
+					z = radius_ref * Mathf.Cos (phi+Mathf.PI / followMouseSpeed);
+					x = radius_ref * Mathf.Sin (phi+Mathf.PI / followMouseSpeed);
 				}
 				else
 				{
-					x = radius_ref * Mathf.Cos (phi-Mathf.PI / followMouseSpeed);
-					y = radius_ref * Mathf.Sin (phi-Mathf.PI / followMouseSpeed);
+					z = radius_ref * Mathf.Cos (phi-Mathf.PI / followMouseSpeed);
+					x = radius_ref * Mathf.Sin (phi-Mathf.PI / followMouseSpeed);
 				}
 			}
 		} else {
-			
-			x = radius_ref * Mathf.Cos (phiMouse);
-			y = radius_ref * Mathf.Sin (phiMouse);
+
+			z = radius_ref * Mathf.Cos (phiMouse);
+			x = radius_ref * Mathf.Sin (phiMouse);
 		}
 
-		transform.position = new Vector3 (x, y, transform.position.z);
+		transform.position = new Vector3 (x, transform.position.y, z);
 	
 	}
 	// Use this for initialization
 	void Start () {
-		radius_ref = Mathf.Sqrt (Mathf.Pow (transform.position.x, 2) + Mathf.Pow (transform.position.y, 2));
+		radius = Mathf.Sqrt (Mathf.Pow (transform.position.x, 2) + Mathf.Pow (transform.position.z, 2));
 	}
-
+	void Update()
+	{
+		dist = distance;
+		//radius_ref = Mathf.Sqrt (Mathf.Pow (transform.position.x, 2) + Mathf.Pow (transform.position.z, 2));
+	
+	}
 }
