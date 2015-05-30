3833cec6ddd8a003197fd2ca9846e0b87319d529
vinzynth <vinzynthnihilit@gmail.com>
2015-05-30 17:11:28 +0000

    function

--- matlab/population_function.m
+++ matlab/population_function.m
@@ -7,7 +7,7 @@ N = ones(max(p(3), p(4)) +1 , 1);
 
 % iteration
 for t = size(N,1):t_max-1
-    N = [N; max(N(t) + p(1)* N(t-p(3)) * (K(t) - N(t-p(4)))/K(t), 1)];
+    N = [N; max(N(t) + p(1)* N(t-p(3)) * (K(t) - N(t-p(4)))/K(t), 0) + 1];
 end
 
 end
