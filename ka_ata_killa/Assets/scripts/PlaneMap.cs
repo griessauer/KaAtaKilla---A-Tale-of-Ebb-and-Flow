383884b7097cfea94d8f1ffd8ea37840769b039e
David Griessauer <david.griessauer@gmail.com>
2015-05-30 23:42:40 +0200

    Added PlaneMapping script

---
+++ ka_ata_killa/Assets/scripts/PlaneMap.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3 size = GameObject.Find("RefPlane").GetComponent<Renderer>().bounds.size;
+        float step = 1;
+        int arraySize = (int)Mathf.Floor((float)(size.x/step));
+        double[,] map = new double[arraySize,arraySize];
+
+        for (int i = 0; i < arraySize; i ++)
+        {
+            for (int j = 0; j < arraySize; j++)
+            {
+
+                Ray ray = new Ray(new Vector3((i * step)-arraySize/2, 0f, (j * step)-arraySize/2), new Vector3(0, -1, 0));
+                // create a plane at 0,0,0 whose normal points to +Y:
+                Collider planeCollider = GameObject.Find("FlatEarth").GetComponent<Collider>();
+                
+                // Plane.Raycast stores the distance from ray.origin to the hit point in this variable:
+                RaycastHit distance = new RaycastHit();
+                // if the ray hits the plane...
+                if (planeCollider.Raycast(ray, out distance, 200f))
+                {
+                     
+                    map[i, j] = -distance.point.y;
+                                          
+                }
+                else
+                {
+                    map[i, j] = -1;
+                }
+            }
+
+
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
