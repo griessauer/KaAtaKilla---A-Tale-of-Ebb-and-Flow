fba3b9fa525bddad58cebe4157091a386e8606d3
David Griessauer <david.griessauer@gmail.com>
2015-05-31 13:47:44 +0200

    Started House Placement
    NOT FINISHED

--- ka_ata_killa/Assets/scripts/HousePlacement.cs
+++ ka_ata_killa/Assets/scripts/HousePlacement.cs
@@ -15,18 +15,46 @@ public class HousePlacement : MonoBehaviour {
 			GameObject.Find ("Moon").GetComponent<MeshRenderer> ().material.mainTexture = tex;
 		}
 	}
+    static private GameObject[,] local_planes;
 	
 	// Update is called once per frame
 	void Update () {
+        
+        
+	}
+    public static void PlaceHouses(float[,] population)
+    {
+       if(local_planes == null)
+       {
+           local_planes = new GameObject[PlaneMap.map.GetLength(0),PlaneMap.map.GetLength(1)];
+           float temp_step = PlaneMap.step;
+           for (int i = 0; i < PlaneMap.map.GetLength(0); i++)
+           {
+               for (int j = 0; j < PlaneMap.map.GetLength(1); j++)
+               {
+                   Vector3 temp = new Vector3(i*temp_step,0,j*temp_step);
+                   GameObject temp_plane = GameObject.CreatePrimitive(PrimitiveType.Plane);       
+                   temp_plane.transform.position = temp;
+                   Texture testTexture = new Texture();
+                   //temp_plane.renderer.GetComponent<Renderer>().material.SetTexture()  ;
+
+                   local_planes[i, j] = temp_plane;
+               }
+           }   
+       }
+        
         for (int i = 0; i < PlaneMap.map.GetLength(0); i++)
         {
             for (int j = 0; j < PlaneMap.map.GetLength(1); j++)
             {
-
-
-
+                local_planes[i,j] = GameObject.CreatePrimitive(PrimitiveType.Plane);
                 
+                
+
+
+
             }
         }   
-	}
+        
+    }
 }
